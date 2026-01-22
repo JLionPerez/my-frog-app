f76ad4c6c245901a7818b1b5f1dd8805d8b9d3ca
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2026-01-21 16:41:04 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <div className="header"></div>
+      <div className="body"></div>
+      <div className="footer"></div>
+    </div>
+  )
+}
+
+export default App

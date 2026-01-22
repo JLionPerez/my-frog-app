1a9a9c1e0716c02b772942d2ed73e2427bfec91a
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2026-01-21 17:48:33 -0800

    I have no idea what I'm doing
    Trying to link another file to App and it's not working
    maybe it's the poor internet connection, but I will try again
    to make the contents of another file appear when linked to App.

---
+++ pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+// import './App.css'
+import { link } from 'react-router-dom'
+
+function Home(){
+    return (
+        <div>
+            <p>This is the Home page.</p>
+        </div>
+    )
+}
+
+export default Home
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import React from "react"
+import Home from '../pages/Home'
 import './App.css'
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="container">
-      <div className="header"></div>
-      <div className="body"></div>
-      <div className="footer"></div>
+    <div>
+      <h1>Hello</h1>
+        <Home />
     </div>
+
+    // <div className="container">
+    //   <div className="header">
+    //     <div className="bar">
+    //       <div className="logo">Logo</div>
+    //       <div className="search">search</div>
+    //       <div className="cart">Cart</div>
+    //       <div className="account">Account</div>
+    //     </div>
+    //     <div className="navbar">
+    //       <div className="home">Home</div>
+    //       <div className="categories">Categories</div>
+    //       <div className="contact">Contact Me</div>
+    //       <div className="guestbook">Guestbook</div>
+    //     </div>
+    //   </div>
+    //   <div className="body"></div>
+    //   <div className="footer"></div>
+    // </div>
   )
 }
 

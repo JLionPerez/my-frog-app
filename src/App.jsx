99400985d03c4020110d92fdf72912ef594c9d7a
JLionPerez <49221382+JLionPerez@users.noreply.github.com>
2026-01-28 22:43:12 -0800

    Add nav html

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,17 @@ import Home from "./pages/home"
 import About from "./pages/about"
 import Guestbook from "./pages/guestbook"
 
-export default function App() {
+function App() {
   return (
-
+    <Router>
+      {/* <Navbar /> */}
+      <nav>
+        <Link to="./pages/home">Home</Link>
+      </nav>
+      <Routes>
+        <Route path="./pages/home" element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
+export default App;
